de6103f7691e8ebe401a603b28570144bf375048
Nicolas Steven Miller <nicolasmiller@gmail.com>
2014-09-26 23:47:03 -0700

    .

--- Scripts/loader.js
+++ Scripts/loader.js
@@ -5,7 +5,9 @@ function receiveMessage(event)
         $.getJSON('http://whateverorigin.org/get?url=' + url + '&callback=?', function(data){
             var html = "" + data.contents;
             
+            console.log(html);
             html = html.replace(new RegExp('(href|src)="/', 'g'),  '$1="' + url + '/');
+            console.log(html);
 
             $("#siteLoader").html(html);
         });
