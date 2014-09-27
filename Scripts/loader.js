8aaf8cb854eaefa657c2cf3dd54feb1ee0fbdd16
Nicolas Steven Miller <nicolasmiller@gmail.com>
2014-09-26 23:39:42 -0700

    .

--- Scripts/loader.js
+++ Scripts/loader.js
@@ -1,10 +1,11 @@
 function receiveMessage(event)
 {
+    var url = event['data'];
     if(event.origin === "http://nicolasmiller.github.io") {
-        $.getJSON('http://whateverorigin.org/get?url=' + event['data'] + '&callback=?', function(data){
+        $.getJSON('http://whateverorigin.org/get?url=' + url + '&callback=?', function(data){
             var html = "" + data.contents;
             
-            html = html.replace(new RegExp('(href|src)="/', 'g'),  '$1="'+url+'/');
+            html = html.replace(new RegExp('(href|src)="/', 'g'),  '$1="' + url + '/');
 
             $("#siteLoader").html(html);
         });
